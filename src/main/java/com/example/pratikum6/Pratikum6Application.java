1833e0af5fda474c3d9be7ff836a8f4eaddc6ec7
danny fadli <danyfdly95@gmail.com>
2026-04-21 10:54:25 +0700

    MEMBUAT PRATIKUM 6

---
+++ src/main/java/com/example/pratikum6/Pratikum6Application.java
@@ -0,0 +1,13 @@
+package com.example.pratikum6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratikum6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratikum6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pratikum6/Pratikum6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pratikum6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratikum6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
